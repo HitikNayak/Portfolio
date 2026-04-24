80d6ed6a728c44bbe50b39476b19fc24bb135189
Hitik Kumar Nayak <96581539+HitikNayak@users.noreply.github.com>
2026-04-24 18:30:13 +0530

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,117 +1,32 @@
-/* =========================
-   VLSI Portfolio Service Worker
-========================= */
+const CACHE_NAME = "vlsi-cache-v1";
 
-const CACHE_NAME = "vlsi-portfolio-v1";
+const FILES = [
 
-/* Files to cache */
-
-const FILES_TO_CACHE = [
-
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./manifest.json",
-  "./cv.pdf",
-
-  /* Images */
-
-  "./images/schematic.png",
-  "./images/layout.png",
-  "./images/lvs.png",
-  "./images/pex.png",
-  "./images/iopadring.png",
-  "./images/testbench.png",
-  "./images/waveform.png",
-  "./images/postlayout.png",
-
-  /* Icons */
-
-  "./icons/android-icon-36x36.png",
-  "./icons/android-icon-48x48.png",
-  "./icons/android-icon-72x72.png",
-  "./icons/android-icon-96x96.png",
-  "./icons/android-icon-144x144.png",
-  "./icons/android-icon-192x192.png"
+"./",
+"./index.html",
+"./styles.css",
+"./manifest.json"
 
 ];
 
-/* =========================
-   Install Event
-========================= */
+self.addEventListener("install", e => {
 
-self.addEventListener("install", event => {
+e.waitUntil(
 
-  console.log("Service Worker Installing...");
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(FILES))
 
-  event.waitUntil(
-
-    caches.open(CACHE_NAME)
-      .then(cache => {
-
-        console.log("Caching Files");
-
-        return cache.addAll(FILES_TO_CACHE);
-
-      })
-
-  );
+);
 
 });
 
-/* =========================
-   Fetch Event
-========================= */
+self.addEventListener("fetch", e => {
 
-self.addEventListener("fetch", event => {
+e.respondWith(
 
-  event.respondWith(
+caches.match(e.request)
+.then(res => res || fetch(e.request))
 
-    caches.match(event.request)
-      .then(response => {
-
-        if (response) {
-          return response;
-        }
-
-        return fetch(event.request);
-
-      })
-
-  );
-
-});
-
-/* =========================
-   Activate Event
-========================= */
-
-self.addEventListener("activate", event => {
-
-  console.log("Service Worker Activating...");
-
-  event.waitUntil(
-
-    caches.keys().then(cacheNames => {
-
-      return Promise.all(
-
-        cacheNames.map(cache => {
-
-          if (cache !== CACHE_NAME) {
-
-            console.log("Deleting old cache:", cache);
-
-            return caches.delete(cache);
-
-          }
-
-        })
-
-      );
-
-    })
-
-  );
+);
 
 });
